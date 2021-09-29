c48e4cacfc0bab323548dc70a1e3a6e88919fe77
xSuffix <xSuffix@users.noreply.github.com>
2021-09-29 08:33:58 +0000

    deploy: 6a50f72af5b5d29035fb30560ba7cabbfa6529c7

---
+++ _nuxt/static/1632904432/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632904432",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},_img:{"/_ipx/_/banner.webp":"\u002F_nuxt\u002Fimages\u002Fbanner-50fd7f.webp"}}}(null,"\u002F"));
