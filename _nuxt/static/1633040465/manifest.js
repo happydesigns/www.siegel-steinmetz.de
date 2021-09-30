eb76a01050b51fa4ad7dcfb8b73a631d7c4566c8
xSuffix <xSuffix@users.noreply.github.com>
2021-09-30 23:50:36 +0000

    deploy: 23ad9e0e78db8f436ec843b7c6f4d7b22f241c4f

--- _nuxt/static/1633040465/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fgalerie\u002Fgrabmalreferenzen","\u002Fgrabmale","\u002Fdatenschutz","\u002Fimpressum","\u002Frestaurierungen","\u002Fkontakt","\u002Fueber-uns","\u002Fgalerie\u002Fgrabmallager","galerie\u002Frestaurierungen","\u002Fgalerie","\u002Fgalerie\u002Frestaurierungen"]})
