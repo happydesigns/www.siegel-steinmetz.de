0c7007af2fc746b982ca05c97aa7cb37e0c1732c
zanfee <zanfee@users.noreply.github.com>
2023-02-09 22:41:17 +0000

    deploy: 7f5ae8d27904c25f3e77579a75fdeabf2aed4443

---
+++ _nuxt/static/1675982471/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fkontakt","\u002F","\u002Fgalerie\u002Fgrabmallager","\u002Fgrabmale","\u002Fdatenschutz","\u002Fimpressum","\u002Frestaurierungen","\u002Fueber-uns","\u002Fgalerie\u002Fgrabmalreferenzen","galerie\u002Frestaurierungen","\u002Fgalerie","\u002Fgalerie\u002Frestaurierungen"]})
